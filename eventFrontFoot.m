39a98e4415f7f2f2a23cc546b805157f183d4f74
L-zl <lzl12051@users.noreply.github.com>
2021-04-13 20:47:17 -0700

    add small test case

--- eventFrontFoot.m
+++ eventFrontFoot.m
@@ -10,3 +10,11 @@ function [value,isterminal,direction] = eventFrontFoot(t,x)
     isterminal = 1;   % stop the integration
     direction = 0;   % negative direction -1
 end
+
+% samll test case
+
+% global h_ground
+% h_ground = [0.1,0.2,0.5,0.5,0.6]
+% t = 0.1
+% x = [0;0.4;0.1;pi/4;pi/2;pi/4;pi/2]
+% eventFrontFoot(t,x)
